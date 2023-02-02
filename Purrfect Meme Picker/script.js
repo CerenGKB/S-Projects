1e893704d3da08e88545d9f31333bdb28b08c241
CerenGKB <cerengokbulut@hotmail.com>
2023-02-02 23:32:57 +0300

    Added a responsive design page

---
+++ Purrfect Meme Picker/script.js
@@ -0,0 +1,16 @@
+// there is my data jsonlike format, add it to a different file called data.js
+// Dont forget to export and import, add type= module in your script.js in html file 
+
+// 2 for of loop that returns an array of emotions from my data array called X
+
+// Rendering radio buttons that is returning my function X
+
+// rendering text example:
+//    function renderEmotionsRadios(cats){
+//     let html = ''
+//     const emotions = getEmotionsArray(cats)
+//     for(let emotion of emotions){
+//       html +=   `<p>${emotion}</p>`
+//     }
+//     emotionRadios.innerHTML = html
+// }
