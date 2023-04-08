ceaca2accde4acfdddb27626c6d3a0c401d04119
CerenGKB <cerengokbulut@hotmail.com>
2023-04-08 13:06:53 +0300

    Created a order section

--- Coffee ordering app/coffeeData.js
+++ Coffee ordering app/coffeeData.js
@@ -2,22 +2,22 @@ export const coffeeArray = [
     {
         name: "Filter Coffee",
         ingredients: "Water,Coffee",
-        id: 0,
-        price: '$10',
+        id: "0",
+        price: 10,
         emoji: `images/filterCoffee.png`
     },
     {
         name:"Latte",
         ingredients: "Espresso,Milk of your choice",
-        id:1,
-        price: '$15',
+        id: "1",
+        price: 15,
         emoji: `images/latte.png`
     },
     {
         name:"Cappucino",
         ingredients: "Espresso,Milk,Foam",
-        id:2,
-        price: '$16',
+        id: "2",
+        price: 16,
         emoji: `images/cappucino.png`
     }
 ]
--- Coffee ordering app/script.js
+++ Coffee ordering app/script.js
@@ -1,11 +1,93 @@
 import { coffeeArray } from "./coffeeData.js"
 
+const orderTitle = document.getElementById('orderTitle')
+orderTitle.style.display = "none"
 
 
-document.addEventListener('click',function(e){
-    let orderArr = ''
-    console.log(e.target.dataset.plus)
+let orderArr = []
+
+// add event listener to the plus icons
+document.addEventListener('click', function(e) {
+ addItem(e)
+// -------------ADD ITEM ---------
+    
+
+ //  --------- REMOVE ITEM ----------
+ if (e.target.dataset.remove) {
+    // extract the index of the clicked item
+    const index = e.target.dataset.remove;
+    // find the item in the orderArr
+    const removedCoffee = orderArr[index];
+    // decrease the quantity and update the price for each click on the "Remove" button
+    if (removedCoffee.quantity > 1) {
+        removedCoffee.quantity--;
+        removedCoffee.price -= removedCoffee.price / (removedCoffee.quantity + 1);
+    } else {
+        // if the quantity is 1, remove the item from the orderArr
+        orderArr.splice(index, 1);
+    }
+    // update the order HTML
+    const order = document.getElementById('order');
+    order.innerHTML = getOrderHtml();
+  }
 })
+
+function addItem(e){
+    if (e.target.dataset.plus) {
+        // extract the id number of the clicked coffee
+        const idNumber = e.target.dataset.plus
+        // find the corresponding coffee object in the coffeeArray
+        const clickedCoffee = coffeeArray.find(coffee => coffee.id === idNumber)
+        // extract the name and price of the clicked coffee
+        const clickedCoffeeName = clickedCoffee.name
+        const clickedCoffeePrice = clickedCoffee.price
+        // check if the clicked coffee is already in the orderArr
+        const existingCoffee = orderArr.find(coffee => coffee.name === clickedCoffeeName)
+        if (existingCoffee) {
+            // if it is, increment the quantity and update the price
+            existingCoffee.quantity++
+            existingCoffee.price  += clickedCoffeePrice
+        } else {
+            // if it isn't, add a new object to the orderArr
+            orderArr.push({
+                name: clickedCoffeeName,
+                quantity: 1,
+                price: clickedCoffeePrice,
+                originalPrice: clickedCoffeePrice
+            })
+        }
+        console.log(orderArr)
+        
+        orderTitle.style.display = "block"
+       const order = document.getElementById('order')
+       order.innerHTML = getOrderHtml()
+    }
+
+}
+
+
+function getOrderHtml(){
+    let orderHtml = ''
+
+    orderArr.forEach((coffee,index) => {
+        orderHtml += `
+        
+        <div class="orderBar">
+       <div class="orderText">
+            <div class="orderName">${coffee.name} 
+            </div>
+        <div class="removeButton">
+            <button data-remove="${index}">Remove</button>
+            </div>
+        </div>
+        <div class="orderPrice">$${coffee.price}</div>
+        </div>
+        `
+    })
+    return orderHtml
+
+}
+
 
 
 function getFeedHtml(){
@@ -22,9 +104,9 @@ function getFeedHtml(){
             <div class="coffeeDetails">
             <p class="coffeeName">${coffee.name}</p> 
             <p class="coffeeIngredient">${coffee.ingredients}</p> 
-            <p class="coffeePrice">${coffee.price}</p> 
+            <p class="coffeePrice">$${coffee.price}</p> 
             </div>
-           <div class="plusButton"> <i class="fa-solid fa-circle-plus button" data-plus=${coffee.id} style="color: #8d4002;"></i></div>
+           <div class="plusButton" > <i class="fa-solid fa-circle-plus button" data-plus=${coffee.id} style="color: #8d4002;"></i></div>
            </div>
         </div>
     </div>`
@@ -34,7 +116,7 @@ function getFeedHtml(){
 
 function render(){
     const feed = document.getElementById('feed')
-    feed.innerHTML = getFeedHtml()
+    feed.innerHTML =  getFeedHtml()
 }
 
 render()
